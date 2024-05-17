3226ab3b286501e84b58b7ddf2bd27ae1598341a
Romy <mail@romy.dev>
2024-05-17 14:18:37 +0200

    odpr 6

--- ConsoleMonGame/ConsoleMonGame/ConsoleMon.cs
+++ ConsoleMonGame/ConsoleMonGame/ConsoleMon.cs
@@ -12,7 +12,7 @@ namespace ConsoleMonGame
         internal int energy;
         internal string name;
 
-        private List<Skill> skills;
+        private List<Skill> skills = new List<Skill>();
 
         internal void TakeDamage(int damage)
         {
---
+++ ListOpdracht/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ListOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> birds = new List<string>() { "uil", "kraai", "hond", "papegaai" };
+            List<string> birds2 = new List<string>() { "meeuw", "duif" };
+
+            birds.AddRange(birds2);
+            birds.Remove("hond");
+
+            for(int i = 0; i < birds.Count; i++)
+            {
+                Console.WriteLine($"{birds[i]}");
+            }
+        }
+
+    }
+}
