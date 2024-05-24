95fe37ca0a33c49782e6f0ef3863ea55a531eff3
Romy <mail@romy.dev>
2024-05-24 14:30:29 +0200

    opdr 7

---
+++ EnumOpdracht/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EnumOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Colors myMainThemeColor = Colors.pink;
+
+            if (myMainThemeColor == Colors.pink)
+            {
+                Console.WriteLine("Dat is mijn kleur!");
+            }
+        }
+    }
+}
