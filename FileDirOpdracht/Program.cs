6da75fbb0d1452b7a92cc7c200bb21c07993960e
Romy <mail@romy.dev>
2024-05-27 10:38:01 +0200

    opdr 9

---
+++ FileDirOpdracht/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\romyvdhx\Documents\ma\bewijzenmap\p1.4\prog\2023P4ProgCs2");
+
+            System.IO.FileInfo[] files = dir.GetFiles();
+            System.IO.DirectoryInfo[] directories = dir.GetDirectories();
+            for (int i = 0; i < directories.Length; i++) 
+            {
+                Console.WriteLine(directories[i]);
+            }
+        }
+    }
+}
